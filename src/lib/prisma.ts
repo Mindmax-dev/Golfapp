cafa4e490974dc165a77ccd4e5ec2b79363fe378
Max Beutler <mindmax2016@gmail.com>
2026-05-16 13:24:21 +0200

    asdf

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,13 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! }, { schema: "golf" });
+  const adapter = new PrismaPg(
+    {
+      connectionString: process.env.DATABASE_URL!,
+      ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    },
+    { schema: "golf" }
+  );
   return new PrismaClient({ adapter });
 }
 
